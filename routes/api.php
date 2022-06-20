7afa9639d7bcffaaa0e4423e6281ad3f52440b4b
oussama <oussamaeessid@gmail.com>
2022-06-20 17:59:37 +0200

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+ //  return $request->user();
+//});
+
+Route::apiResource('/product','App\Http\Controllers\ProductController');
+
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews','App\Http\Controllers\ReviewController');
 });
